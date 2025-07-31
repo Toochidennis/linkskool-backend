84d6e79808ccd6e9a0e852d381b1a267697fadae
Toochidennis <dennistoochukwu@gmail.com>
2025-07-31 19:12:39 +0100

    bug fixes

---
+++ v3/app/Database/db_schema.php
@@ -0,0 +1,39 @@
+<?php
+
+$schema = [
+    'account_chart' => [
+        'typeid' => [
+            'type' => 'int(10) UNSIGNED',
+            'nullable' => false,
+            'auto_increment' => true,
+            'primary' => true,
+        ],
+        'account_id' => [
+            'type' => 'varchar(50)',
+            'nullable' => true,
+            'default' => null,
+        ],
+        'account_type' => [
+            'type' => 'int(11)',
+            'nullable' => true,
+            'default' => null,
+        ],
+        'account_name' => [
+            'type' => 'varchar(255)',
+            'nullable' => true,
+            'default' => null,
+        ],
+        'inactive' => [
+            'type' => 'varchar(10)',
+            'nullable' => true,
+            'default' => null,
+        ],
+        '__meta' => [
+            'engine' => 'InnoDB',
+            'charset' => 'latin1',
+            'collate' => 'latin1_swedish_ci',
+        ],
+    ],
+
+    
+];
