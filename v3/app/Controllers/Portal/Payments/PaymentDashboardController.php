c87657cccf0ed1eb76787b1746f7eba18fd14546
Toochidennis <dennistoochukwu@gmail.com>
2025-08-18 18:33:06 +0100

    bug fixes

---
+++ v3/app/Controllers/Portal/Payments/PaymentDashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers\Portal\Payments;
+
+use V3\App\Controllers\BaseController;
+
+class PaymentDashboardController extends BaseController
+{
+    // Controller methods for handling payment dashboard functionalities
+    // This class will contain methods to manage payments, view payment history, etc.
+}
