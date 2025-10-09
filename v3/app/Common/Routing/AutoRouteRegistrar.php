3d6423597fcae50436f31c60f8937be92f9cef42
ToochiDennis <dennistoochukwu@gmail.com>
2025-10-09 18:09:47 +0100

    new features

--- v3/app/Common/Routing/AutoRouteRegistrar.php
+++ v3/app/Common/Routing/AutoRouteRegistrar.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace V3\App\Common\Routing;
 
 use ReflectionClass;
@@ -6,15 +7,34 @@ use ReflectionMethod;
 
 class AutoRouteRegistrar
 {
-    public function __construct(private CustomRouteCollector $r) {}
+    private string $cacheFile;
 
-    public function registerControllers(string $namespace, string $prefix = ''): void
+    public function __construct(private CustomRouteCollector $r)
     {
+    }
+
+    public function registerControllers(string $namespace, string $prefix = '', bool $useCache = true): void
+    {
+        if ($useCache && file_exists($this->cacheFile)) {
+            $routes = include $this->cacheFile;
+            foreach ($routes as $route) {
+                $this->r->addRoute(
+                    $route['method'],
+                    $route['path'],
+                    [$route['class'], $route['methodName']],
+                    $route['middleware']
+                );
+            }
+            return;
+        }
+
         $path = __DIR__ . '/../../../Controllers/' . str_replace('\\', '/', basename($namespace));
 
         $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($path));
         foreach ($iterator as $file) {
-            if (!$file->isFile() || !str_ends_with($file->getFilename(), 'Controller.php')) continue;
+            if (!$file->isFile() || !str_ends_with($file->getFilename(), 'Controller.php')) {
+                continue;
+            }
 
             $className = $namespace . '\\' . str_replace(
                 ['/', '.php'],
@@ -22,7 +42,9 @@ class AutoRouteRegistrar
                 substr($file->getPathname(), strlen($path) + 1)
             );
 
-            if (!class_exists($className)) continue;
+            if (!class_exists($className)) {
+                continue;
+            }
 
             $ref = new ReflectionClass($className);
             foreach ($ref->getMethods(ReflectionMethod::IS_PUBLIC) as $method) {
@@ -38,6 +60,23 @@ class AutoRouteRegistrar
                     );
                 }
             }
+        }
+    }
+
+    private function storeCache(array $routes): void
+    {
+        $content = '<?php return ' . var_export($routes, true) . ';';
+        $dir = dirname($this->cacheFile);
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        file_put_contents($this->cacheFile, $content);
+    }
+
+    public function clearCache(): void
+    {
+        if (file_exists($this->cacheFile)) {
+            unlink($this->cacheFile);
         }
     }
 }
