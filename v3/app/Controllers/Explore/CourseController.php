2b5811d770d5e300acc303ee4b00027cf589c76a
ToochiDennis <dennistoochukwu@gmail.com>
2025-11-20 14:22:20 +0100

    Created course controller

---
+++ v3/app/Controllers/Explore/CourseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace V3\App\Controllers\Explore;
+
+use V3\App\Common\Routing\Group;
+
+#[Group('/public/cbt')]
+class CourseController extends ExploreBaseController
+{
+    // CourseController implementation goes here
+}
