d1aa24fc5f88c7e35ddc915266090f6dc52402f0
ToochiDennis <dennistoochukwu@gmail.com>
2025-12-02 11:31:40 +0100

    modification

--- v3/app/Services/Explore/ExamService.php
+++ v3/app/Services/Explore/ExamService.php
@@ -3,6 +3,7 @@
 namespace V3\App\Services\Explore;
 
 use PDO;
+use V3\App\Common\Enums\QuestionType;
 use V3\App\Common\Utilities\FileHandler;
 use V3\App\Models\Explore\Exam;
 use V3\App\Models\Explore\AuditLog;
@@ -123,10 +124,71 @@ class ExamService
             'course_name' => $settings['course_name'],
             'description' => $settings['description'] ?? '',
             'url' => $this->json($questionIds),
+            'upload_date' => date('Y-m-d H:i:s'),
             'year' => $year,
         ];
 
         return $this->exam->insert($payload);
+    }
+
+    public function getExams(int $page = 1, $limit = 25): array
+    {
+        return $this->exam
+            ->select(['id', 'course_name',, 'year', 'upload_date'])
+            ->orderBy('course_name', 'DESC')
+            ->paginate($limit, $page);
+    }
+
+    public function getQuestions(int $examId): array
+    {
+        $exam = $this->exam
+            ->select(['url'])
+            ->where('id', '=', $examId)
+            ->first();
+
+        if (!$exam) {
+            return [];
+        }
+
+        $questionIds = json_decode($exam['url'], true, 512, JSON_THROW_ON_ERROR);
+
+        if (empty($questionIds) || !\is_array($questionIds)) {
+            return [];
+        }
+
+        $questions = $this->quiz
+            ->select([
+                'question_id',
+                'title AS question_text',
+                'content AS question_files',
+                'topic',
+                'topic_id',
+                'passage',
+                'passage_id',
+                'instruction',
+                'instruction_id',
+                'explanation',
+                'explanation_id',
+                'type as question_type',
+                'answer as options',
+                'correct',
+            ])
+            ->in('question_id', $questionIds)
+            ->get();
+
+        if (empty($questions)) {
+            return [];
+        }
+
+        $questions = array_map(function ($question) {
+            $question['question_type'] = QuestionType::tryFrom($question['question_type'])?->label() ?? 'Unknown';
+            $question['question_files'] = $this->decode($question['question_files']);
+            $question['options'] = $this->decode($question['options']);
+            $question['correct'] = $this->decode($question['correct']);
+            return $question;
+        }, $questions);
+
+        return $questions;
     }
 
     private function logAction(
@@ -157,5 +219,15 @@ class ExamService
             $data,
             JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
         );
+    }
+
+    private function decode(?string $data): array
+    {
+        if (empty($data) || $data === null) {
+            return [];
+        }
+
+        $decoded = json_decode($data, true);
+        return \is_array($decoded) ? $decoded : [];
     }
 }
